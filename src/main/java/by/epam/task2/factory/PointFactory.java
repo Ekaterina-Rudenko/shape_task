9ca768b3279af096ad5246624a1271748ab30afa
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 00:50:12 +0300

    Update PointFactory.java

--- src/main/java/by/epam/task2/factory/PointFactory.java
+++ src/main/java/by/epam/task2/factory/PointFactory.java
@@ -2,8 +2,6 @@ package by.epam.task2.factory;
 
 import by.epam.task2.entity.Point;
 
-public class PointFactory {
-    public Point createPoint(double x, double y, double z){
-        return new Point(x, y, z);
-    }
+public interface PointFactory {
+    Point createPoint(double x, double y, double z);
 }
