c74b6c086fd7d8e790c5b17cd49e02a39d27eee8
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:22:02 +0300

    Update ConeCalculationImpl.java

--- src/main/java/by/epam/task2/service/impl/ConeCalculationImpl.java
+++ src/main/java/by/epam/task2/service/impl/ConeCalculationImpl.java
@@ -2,12 +2,16 @@ package by.epam.task2.service.impl;
 
 import by.epam.task2.entity.Cone;
 import by.epam.task2.exception.CustomException;
+import by.epam.task2.repository.Repository;
 import by.epam.task2.service.ConeCalculation;
 import by.epam.task2.validator.ConeValidator;
 import by.epam.task2.validator.impl.ConeValidatorImpl;
+import by.epam.task2.warehouse.Warehouse;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 
 public class ConeCalculationImpl implements ConeCalculation {
@@ -51,6 +55,19 @@ public class ConeCalculationImpl implements ConeCalculation {
         boolean onPlane = (cone.getCentrePoint().getX() == 0 && cone.getCentrePoint().getY() == 0);
         logger.log(Level.INFO, "The cone base is on the coordinates plane - " + onPlane);
         return onPlane;
+    }
+
+    @Override
+    public double averageSurfaceArea() throws CustomException {
+        Repository repository = Repository.getInstance();
+        Warehouse warehouse = Warehouse.getInstance();
+        double totalSurface = 0;
+        List<Cone> cones = repository.getCones();
+        for (Cone c : cones) {
+            totalSurface += warehouse.getCone(c.getConeId()).getSurfaceArea();
+        }
+        double averageSurface = totalSurface / cones.size();
+        return averageSurface;
     }
 
     private double findHeight(Cone cone) {
