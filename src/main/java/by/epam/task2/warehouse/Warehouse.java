94c3a541a6e18bba7390d7a1e01c1e9a9ece0799
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:52:53 +0300

    Update Warehouse.java

--- src/main/java/by/epam/task2/warehouse/Warehouse.java
+++ src/main/java/by/epam/task2/warehouse/Warehouse.java
@@ -1,6 +1,5 @@
 package by.epam.task2.warehouse;
 
-import by.epam.task2.exception.CustomException;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,31 +12,35 @@ public class Warehouse {
     private static Warehouse instance;
     private Map<Long, ConeParameter> coneMap;
 
-    public static Warehouse getInstance(){
-        if(instance == null){
+    public static Warehouse getInstance() {
+        if (instance == null) {
             instance = new Warehouse();
         }
         return instance;
     }
-    private Warehouse(){
+
+    private Warehouse() {
         this.coneMap = new HashMap<>();
     }
-    public ConeParameter getCone(long coneId){
+
+    public ConeParameter getCone(long coneId) {
         return coneMap.get(coneId);
     }
-    public void putParameter(long coneId, ConeParameter value){
+
+    public void putParameter(long coneId, ConeParameter value) {
         coneMap.put(coneId, value);
         logger.log(Level.INFO, "Cone " + coneId + " with parameters " + value + " was added");
     }
-    public void removeParameter(long coneId){
+
+    public void removeParameter(long coneId) {
         coneMap.remove(coneId);
         logger.log(Level.INFO, " Cone " + coneId + " was removed");
     }
-    public void putParameter(long coneId, double surfaceArea, double volume){
+
+    public void putParameter(long coneId, double surfaceArea, double volume) {
         ConeParameter coneParameter = new ConeParameter();
         coneParameter.setSurfaceArea(surfaceArea);
         coneParameter.setVolume(volume);
         coneMap.put(coneId, coneParameter);
     }
-
 }
