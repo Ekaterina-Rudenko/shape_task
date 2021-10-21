d27a5f553bad7f04a235be2dfe2d3a15d2405913
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:52:45 +0300

    Update Specification.java

--- src/main/java/by/epam/task2/repository/Specification.java
+++ src/main/java/by/epam/task2/repository/Specification.java
@@ -1,7 +1,6 @@
 package by.epam.task2.repository;
 
 import by.epam.task2.entity.Cone;
-import by.epam.task2.exception.CustomException;
 
 public interface Specification {
     boolean specify(Cone cone);
