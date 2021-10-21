f623cc0226b1b0876eeee406e0a7c814f9d69adc
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:52:19 +0300

    Update CentreZComparator.java

--- src/main/java/by/epam/task2/comparator/CentreZComparator.java
+++ src/main/java/by/epam/task2/comparator/CentreZComparator.java
@@ -5,10 +5,9 @@ import by.epam.task2.entity.Cone;
 import java.util.Comparator;
 
 public class CentreZComparator implements Comparator<Cone> {
-        public int compare (Cone o1, Cone o2){
-            double z1 = o1.getCentrePoint().getZ();
-            double z2 = o2.getCentrePoint().getZ();
-            int compare = Double.compare(z1, z2);
-            return compare;
-        }
+    public int compare(Cone o1, Cone o2) {
+        double z1 = o1.getCentrePoint().getZ();
+        double z2 = o2.getCentrePoint().getZ();
+        return Double.compare(z1, z2);
+    }
 }
