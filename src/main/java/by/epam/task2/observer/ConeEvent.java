d808b68e607adeb4a8586375a2d9edc8d42c7dc2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:52:39 +0300

    Update ConeEvent.java

--- src/main/java/by/epam/task2/observer/ConeEvent.java
+++ src/main/java/by/epam/task2/observer/ConeEvent.java
@@ -4,12 +4,6 @@ import by.epam.task2.entity.Cone;
 import java.util.EventObject;
 
 public class ConeEvent extends EventObject {
-    /**
-     * Constructs a prototypical Event.
-     *
-     * @param source the object on which the Event initially occurred
-     * @throws IllegalArgumentException if source is null
-     */
     public ConeEvent(Cone source) {
         super(source);
     }
