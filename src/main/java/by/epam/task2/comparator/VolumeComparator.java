c4bb556b11448a0bd40d0ef0b93d0e33c13b60c0
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 23:30:53 +0300

    Create VolumeComparator.java

---
+++ src/main/java/by/epam/task2/comparator/VolumeComparator.java
@@ -0,0 +1,17 @@
+package by.epam.task2.comparator;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.service.ConeCalculation;
+import by.epam.task2.service.impl.ConeCalculationImpl;
+
+import java.util.Comparator;
+
+public class VolumeComparator implements Comparator<Cone> {
+    @Override
+    public int compare(Cone cone1, Cone cone2){
+        ConeCalculation calculation = new ConeCalculationImpl();
+        double volume1 = calculation.findVolume(cone1);
+        double volume2 = calculation.findVolume(cone2);
+        return Double.compare(volume1, volume2);
+    }
+}
