2ec48e73bcb4638653efcf77968ed84a48a67e8f
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 23:30:29 +0300

    Create CentreYComparator.java

---
+++ src/main/java/by/epam/task2/comparator/CentreYComparator.java
@@ -0,0 +1,15 @@
+package by.epam.task2.comparator;
+
+import by.epam.task2.entity.Cone;
+
+import java.util.Comparator;
+
+public class CentreYComparator implements Comparator<Cone> {
+    public int compare(Cone o1, Cone o2){
+        double y1 = o1.getCentrePoint().getY();
+        double y2 = o2.getCentrePoint().getY();
+        int compare = Double.compare(y1, y2);
+        return compare;
+    }
+
+}
