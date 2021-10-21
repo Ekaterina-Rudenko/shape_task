c8ae7c0c7659d4ff20a2cb2af060fdd5a4fd1d35
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 23:31:03 +0300

    Update Repository.java

--- src/main/java/by/epam/task2/repository/Repository.java
+++ src/main/java/by/epam/task2/repository/Repository.java
@@ -24,37 +24,44 @@ public class Repository {
         List<Cone> cones = new ArrayList<>();
         return cones;
     }
-    public Optional<Cone> get(int index){
+
+    public Optional<Cone> get(int index) {
         Cone cone = cones.get(index);
         return Optional.ofNullable(cone);
     }
-    public void setCone(int index, Cone cone){
+
+    public void setCone(int index, Cone cone) {
         cones.set(index, cone);
     }
 
     public boolean add(Cone cone) {
         return cones.add(cone);
     }
-    public boolean addAll(Collection <? extends Cone> c){
+
+    public boolean addAll(Collection<? extends Cone> c) {
         return cones.addAll(c);
     }
+
     public boolean remove(Cone cone) {
         return cones.remove(cone);
     }
+
     public boolean removeAll(Collection<Cone> c) {
         return cones.removeAll(c);
     }
-    public List<Cone> query(Specification specification){
+
+    public List<Cone> query(Specification specification) {
         List<Cone> result = new ArrayList<>();
-       result = cones.stream()
-               .filter(specification::specify)
-               .collect(Collectors.toList());
-       return result;
-    }
-    public List<Cone> sortCone(Comparator <? super Cone> comparator){
-        List<Cone> result = cones.stream()
-                .sorted(comparator)
+        result = cones.stream()
+                .filter(specification::specify)
                 .collect(Collectors.toList());
         return result;
+    }
+
+    public void sortCone(Comparator<? super Cone> comparator) {
+        cones.stream()
+                .sorted(comparator)
+                .forEach(o -> toString());
+        return;
     }
 }
