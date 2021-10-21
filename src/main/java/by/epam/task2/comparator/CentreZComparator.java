c6436c7c7490de29a55c1bf6478f82a21e877177
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 23:30:31 +0300

    Create CentreZComparator.java

---
+++ src/main/java/by/epam/task2/comparator/CentreZComparator.java
@@ -0,0 +1,14 @@
+package by.epam.task2.comparator;
+
+import by.epam.task2.entity.Cone;
+
+import java.util.Comparator;
+
+public class CentreZComparator implements Comparator<Cone> {
+        public int compare (Cone o1, Cone o2){
+            double z1 = o1.getCentrePoint().getZ();
+            double z2 = o2.getCentrePoint().getZ();
+            int compare = Double.compare(z1, z2);
+            return compare;
+        }
+}
