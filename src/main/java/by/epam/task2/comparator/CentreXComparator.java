92585820f0ff1ac1804b4d4bdf4bddb607e989d5
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 23:30:09 +0300

    Create CentreXComparator.java

---
+++ src/main/java/by/epam/task2/comparator/CentreXComparator.java
@@ -0,0 +1,14 @@
+package by.epam.task2.comparator;
+
+import by.epam.task2.entity.Cone;
+
+import java.util.Comparator;
+
+public class CentreXComparator implements Comparator<Cone> {
+    public int compare(Cone o1, Cone o2) {
+        double x1 = o1.getCentrePoint().getX();
+        double x2 = o2.getCentrePoint().getX();
+        int comp1 = Double.compare(x1, x2);
+        return comp1;
+    }
+}
