d2f0b83ac6e0cecca37b25d0e2c7537d46bdf3c9
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 02:21:37 +0300

    Update HeightComparator.java

--- src/main/java/by/epam/task2/comparator/HeightComparator.java
+++ src/main/java/by/epam/task2/comparator/HeightComparator.java
@@ -5,9 +5,9 @@ import by.epam.task2.entity.Cone;
 import java.util.Comparator;
 
 public class HeightComparator implements Comparator<Cone> {
-    public int compare(Cone o1, Cone o2){
-        double height1 = Math.abs(o1.getApexPoint().getZ())-Math.abs(o1.getCentrePoint().getZ());
-        double height2 = Math.abs(o2.getApexPoint().getZ())-Math.abs(o2.getCentrePoint().getZ());
+    public int compare(Cone o1, Cone o2) {
+        double height1 = Math.abs(o1.getApexPoint().getZ() - o1.getCentrePoint().getZ());
+        double height2 = Math.abs(o2.getApexPoint().getZ() - o2.getCentrePoint().getZ());
         return Double.compare(height1, height2);
     }
 }
