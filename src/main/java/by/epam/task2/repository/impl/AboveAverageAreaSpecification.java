467a0efe5bc6ce797815e0c5c46d2db943370966
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:21:38 +0300

    Create AboveAverageAreaSpecification.java

---
+++ src/main/java/by/epam/task2/repository/impl/AboveAverageAreaSpecification.java
@@ -0,0 +1,19 @@
+package by.epam.task2.repository.impl;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.exception.CustomException;
+import by.epam.task2.repository.Specification;
+import by.epam.task2.service.ConeCalculation;
+import by.epam.task2.service.impl.ConeCalculationImpl;
+import by.epam.task2.warehouse.Warehouse;
+
+public class AboveAverageAreaSpecification implements Specification {
+    @Override
+    public boolean specify(Cone cone) throws CustomException {
+        Warehouse warehouse = Warehouse.getInstance();
+        ConeCalculation calculation = new ConeCalculationImpl();
+        double averageSurfaceArea = calculation.averageSurfaceArea();
+        double surfaceArea = warehouse.getCone(cone.getConeId()).getSurfaceArea();
+        return (surfaceArea > averageSurfaceArea);
+    }
+}
