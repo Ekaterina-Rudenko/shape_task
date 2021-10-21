0b0e466a314240861e49340e3df0e6fcf32fa657
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-21 04:48:19 +0300

    Create ConeObserverImpl.java

---
+++ src/main/java/by/epam/task2/observer/impl/ConeObserverImpl.java
@@ -0,0 +1,53 @@
+package by.epam.task2.observer.impl;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.exception.CustomException;
+import by.epam.task2.observer.ConeEvent;
+import by.epam.task2.observer.Observer;
+import by.epam.task2.service.ConeCalculation;
+import by.epam.task2.service.impl.ConeCalculationImpl;
+import by.epam.task2.warehouse.ConeParameter;
+import by.epam.task2.warehouse.Warehouse;
+
+public class ConeObserverImpl implements Observer {
+    @Override
+    public void parameterChanged(ConeEvent coneEvent) throws CustomException {
+        Cone cone = coneEvent.getSource();
+        ConeCalculation coneCalculation = new ConeCalculationImpl();
+        double surfaceArea = coneCalculation.findSurfaceArea(cone);
+        double volume = coneCalculation.findVolume(cone);
+
+        Warehouse warehouse = Warehouse.getInstance();
+        long coneId = cone.getConeId();
+        ConeParameter coneParameter = warehouse.getParameter(coneId);
+        coneParameter.setSurfaceArea(surfaceArea);
+        coneParameter.setVolume(volume);
+        warehouse.putParameter(coneId, coneParameter);
+    }
+
+    @Override
+    public void updateSurfaceArea(ConeEvent coneEvent) throws CustomException {
+        Cone cone = coneEvent.getSource();
+        ConeCalculation coneCalculation = new ConeCalculationImpl();
+        double surfaceArea = coneCalculation.findSurfaceArea(cone);
+
+        Warehouse warehouse = Warehouse.getInstance();
+        long coneId = cone.getConeId();
+        ConeParameter coneParameter = warehouse.getParameter(coneId);
+        coneParameter.setSurfaceArea(surfaceArea);
+        warehouse.putParameter(coneId, coneParameter);
+    }
+
+    @Override
+    public void updateVolume(ConeEvent coneEvent) throws CustomException {
+        Cone cone = coneEvent.getSource();
+        ConeCalculation coneCalculation = new ConeCalculationImpl();
+        Warehouse warehouse = Warehouse.getInstance();
+        long coneId = cone.getConeId();
+        ConeParameter coneParameter = warehouse.getParameter(coneId);
+        double volume = coneCalculation.findVolume(cone);
+        coneParameter.setVolume(volume);
+        warehouse.putParameter(coneId, coneParameter);
+    }
+
+}
