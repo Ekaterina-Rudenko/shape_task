4e75cd683f69a980b136be69430c751ed75b1e76
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 01:21:46 +0300

    Create HeightSpecification.java

---
+++ src/main/java/by/epam/task2/repository/impl/HeightSpecification.java
@@ -0,0 +1,20 @@
+package by.epam.task2.repository.impl;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.repository.Specification;
+
+public class HeightSpecification implements Specification {
+    private double minHeight;
+    private double maxHeight;
+
+    public HeightSpecification(double minHeight, double maxHeight) {
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+    }
+
+    @Override
+    public boolean specify(Cone cone) {
+        double height = Math.abs(cone.getApexPoint().getZ()) - Math.abs(cone.getCentrePoint().getZ());
+        return (height <= minHeight && height >= maxHeight);
+    }
+}
