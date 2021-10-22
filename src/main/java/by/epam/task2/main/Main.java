4f24e61f7e90cc48120fbe1113d387bd7cee8662
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 03:53:49 +0300

    Update Main.java

--- src/main/java/by/epam/task2/main/Main.java
+++ src/main/java/by/epam/task2/main/Main.java
@@ -1,14 +1,11 @@
 package by.epam.task2.main;
 
-import by.epam.task2.entity.Cone;
-import by.epam.task2.entity.Point;
 import by.epam.task2.exception.CustomException;
 import by.epam.task2.reader.impl.InfoReaderImpl;
 import by.epam.task2.repository.Repository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -18,7 +15,7 @@ public class Main {
     public static void main(String[] args) throws CustomException {
         Repository repository = Repository.getInstance();
         InfoReaderImpl reader = new InfoReaderImpl();
-        List<String> conesList = reader.readConesFromFile(DEFAULT_FILE_PATH);
+        List<String> conesList = reader.readData(DEFAULT_FILE_PATH);
         System.out.println(conesList.toString());
     }
 }
