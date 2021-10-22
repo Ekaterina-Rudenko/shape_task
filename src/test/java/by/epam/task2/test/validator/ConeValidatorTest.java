92add4c48157874b8d4d34389e41972201b68303
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 05:03:43 +0300

    Create ConeValidatorTest.java

---
+++ src/test/java/by/epam/task2/test/validator/ConeValidatorTest.java
@@ -0,0 +1,115 @@
+package by.epam.task2.test.validator;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.entity.Point;
+import by.epam.task2.validator.ConeValidator;
+import by.epam.task2.validator.impl.ConeValidatorImpl;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ConeValidatorTest {
+    ConeValidator validator;
+    Cone correctCone;
+    Cone wrongCone;
+
+    @BeforeClass
+    public void create() {
+        validator = ConeValidatorImpl.getInstance();
+        correctCone = new Cone(new Point(1, 1, 1), new Point(1, 1, 5), 4.0);
+        wrongCone = new Cone(new Point(100, 1, 1), new Point(1, 1, 5), 4.0);
+    }
+
+    @Test
+    public void checkConeStringTest() {
+        String correctString = "1.0 2.0 0.0 1.0 2.0 6.0 3.5";
+        boolean actual = validator.checkConeString(correctString);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void checkConeStringSecondTest() {
+        String correctString = "1 2 0 1 2 6 3";
+        boolean actual = validator.checkConeString(correctString);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void checkConeStringThirdTest() {
+        String correctString = "1 -2 0 1 -2 -6 3";
+        boolean actual = validator.checkConeString(correctString);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void checkConeStringNegativeTest() {
+        String wrongString = "12334567";
+        boolean actual = validator.checkConeString(wrongString);
+        Assert.assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void checkConeStringrSizeTest() {
+        String wrongString = "1 2 3 3 4 5 6 7 8";
+        boolean actual = validator.checkConeString(wrongString);
+        Assert.assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void isConeDoubleArrayTest() {
+        boolean actual = validator.isCone(1, 1, 2, 1, 1, 10, 4.5);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void isConeDoubleArrayNegativeTest() {
+        boolean actual = validator.isCone(100, 1, 2, 1, 1, 10, -4.5);
+        Assert.assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void isConeDoubleArrayWrongSizeTest() {
+        boolean actual = validator.isCone(100, 1, 2, 1, 1, 10, -4.5, 12, 12, 12, 12, 12);
+        Assert.assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void isConeTest() {
+        Point centre = new Point(1, 1, 2);
+        Point apex = new Point(1, 1, 10);
+        boolean actual = validator.isCone(centre, apex, 4.0);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void isConeNegativeTest() {
+        Point centre = new Point(0, 10, 2);
+        Point apex = new Point(10, 10, 10);
+        boolean actual = validator.isCone(centre, apex, 4.0);
+        Assert.assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void isConeShapeTest() {
+        boolean actual = validator.isCone(correctCone);
+        Assert.assertEquals(actual, true);
+    }
+
+    @Test
+    public void isConeShapeNegativeTest() {
+        boolean actual = validator.isCone(wrongCone);
+        Assert.assertNotEquals(actual, true);
+    }
+    @Test
+    public void checkDissectionHeightTest(){
+        double dissectionHeight = 3.0;
+        boolean actual = validator.checkDissectionHeight(correctCone, dissectionHeight);
+        Assert.assertEquals(actual, true);
+    }
+    @Test
+    public void checkDissectionHeightNegativeTest(){
+        double dissectionHeight = 100.0;
+        boolean actual = validator.checkDissectionHeight(correctCone, dissectionHeight);
+        Assert.assertNotEquals(actual, true);
+    }
+}
