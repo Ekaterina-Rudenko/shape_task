8c09696347bfa377033c1bf1288af96ee0dc8553
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-22 03:53:52 +0300

    Update InfoReaderImpl.java

--- src/main/java/by/epam/task2/reader/impl/InfoReaderImpl.java
+++ src/main/java/by/epam/task2/reader/impl/InfoReaderImpl.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -16,7 +18,13 @@ public class InfoReaderImpl implements InfoReader {
     static Logger logger = LogManager.getLogger();
 
     @Override
-    public List<String> readConesFromFile(String filePath) throws CustomException {
+    public List<String> readData(String path) throws CustomException {
+        ClassLoader loader = getClass().getClassLoader();
+        URL resource = loader.getResource(path);
+        if(resource == null){
+            throw new CustomException("Resource is null " + path);
+        }
+        String filePath = new File(resource.getFile()).getPath();
         List<String> parametersList = new ArrayList<>();
         try {
             Files.lines(Paths.get(filePath))
@@ -25,7 +33,6 @@ public class InfoReaderImpl implements InfoReader {
             logger.log(Level.ERROR, "Reading from " + filePath + " was failed or interrupted", e);
             throw new CustomException("Reading from " + filePath + " was failed or interrupted", e);
         }
-
         return parametersList;
     }
 }
