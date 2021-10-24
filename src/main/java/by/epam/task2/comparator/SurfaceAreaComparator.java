629c919128396dee0c7eebb64fb1564929357db4
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-24 04:53:19 +0300

    Update SurfaceAreaComparator.java

--- src/main/java/by/epam/task2/comparator/SurfaceAreaComparator.java
+++ src/main/java/by/epam/task2/comparator/SurfaceAreaComparator.java
@@ -1,15 +1,16 @@
 package by.epam.task2.comparator;
 
 import by.epam.task2.entity.Cone;
-import by.epam.task2.warehouse.Warehouse;
+import by.epam.task2.service.ConeCalculation;
+import by.epam.task2.service.impl.ConeCalculationImpl;
 
 import java.util.Comparator;
 
 public class SurfaceAreaComparator implements Comparator<Cone> {
     public int compare(Cone cone1, Cone cone2) {
-        Warehouse warehouse = Warehouse.getInstance();
-        double surfaceArea1 = warehouse.getCone(cone1.getConeId()).getSurfaceArea();
-        double surfaceArea2 = warehouse.getCone(cone2.getConeId()).getSurfaceArea();
+        ConeCalculation calculation = new ConeCalculationImpl();
+        double surfaceArea1 = calculation.findVolume(cone1);
+        double surfaceArea2 = calculation.findVolume(cone2);
         return Double.compare(surfaceArea1, surfaceArea2);
     }
 }
