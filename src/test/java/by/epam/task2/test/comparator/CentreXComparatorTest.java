ba40246e7d16cd7d50cf5544833906b0f0828ce1
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-24 04:53:22 +0300

    Update CentreXComparatorTest.java

--- src/test/java/by/epam/task2/test/comparator/CentreXComparatorTest.java
+++ src/test/java/by/epam/task2/test/comparator/CentreXComparatorTest.java
@@ -3,35 +3,47 @@ package by.epam.task2.test.comparator;
 import by.epam.task2.comparator.CentreXComparator;
 import by.epam.task2.entity.Cone;
 import by.epam.task2.entity.Point;
-import by.epam.task2.repository.Repository;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CentreXComparatorTest {
-    Repository repository = Repository.getInstance();
-    CentreXComparator comparator = new CentreXComparator();
-    ArrayList<Cone> cones;
+    CentreXComparator comparatorCentreX;
+
     @BeforeMethod
-    public void setUp(){
-        cones = new ArrayList<>();
-        cones.add(new Cone(new Point(1.0, 2.0, 1.0), new Point(1.0, 2.0, 5.0), 4.0));
-        cones.add(new Cone(new Point(1.0, 3.0, 1.0), new Point(1.0, 2.0, 5.0), 4.0));
-        cones.add(new Cone(new Point(3.0, 1.0, 1.0), new Point(1.0, 2.0, 5.0), 4.0));
-    }
-    @Test
-    public void centreXComparatorTest(){
-        int actual = comparator.compare(cones.get(0), cones.get(1));
-        int expected = 0;
-        Assert.assertEquals(actual, expected);
-    }
-    @Test
-    public void centreXComparatorDifferentTest(){
-        int actual = comparator.compare(cones.get(1), cones.get(2));
-        int expected = -1;
-        Assert.assertEquals(actual, expected);
+    public void create() {
+        comparatorCentreX = new CentreXComparator();
     }
 
+    @DataProvider(name = "conesToCompare")
+    public Object[][] coneData() {
+        List<Cone> coneList = new ArrayList<>();
+        coneList.add(new Cone(new Point(1.0, 2.0, 1.0), new Point(1.0, 2.0, 5.0), 4.0));
+        coneList.add(new Cone(new Point(1.0, 2.0, 1.0), new Point(1.0, 2.0, 5.0), 4.0));
+        coneList.add(new Cone(new Point(3.0, 4.0, 1.0), new Point(3.0, 4.0, 10.0), 14.0));
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(1);
+        expected.add(-1);
+        return new Object[][]{{coneList, expected}};
+    }
+
+    @Test(dataProvider = "conesToCompare")
+    public void centreXComparatorEqualTest(List<Cone> cone, List<Integer> expected) {
+        int actual = comparatorCentreX.compare(cone.get(0), cone.get(1));
+        Assert.assertEquals(actual, (int) expected.get(0));
+    }
+
+    @Test(dataProvider = "conesToCompare")
+    public void centreXComparatorTest(List<Cone> cone, List<Integer> expected) {
+        int actual = comparatorCentreX.compare(cone.get(0), cone.get(2));
+        Assert.assertEquals(actual, (int) expected.get(2));
+    }
 }
+
+
+
