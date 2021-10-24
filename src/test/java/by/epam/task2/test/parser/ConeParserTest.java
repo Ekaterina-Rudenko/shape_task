003d91de2f40da87be8e5fb8f126f62703238d4d
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-25 00:29:48 +0300

    Update ConeParserTest.java

--- src/test/java/by/epam/task2/test/parser/ConeParserTest.java
+++ src/test/java/by/epam/task2/test/parser/ConeParserTest.java
@@ -9,9 +9,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class ConeParserTest {
     ConeParser parser;
@@ -24,13 +22,15 @@ public class ConeParserTest {
     @DataProvider(name = "dataForParsing")
     public Object[][] dataForParsing() {
         List<String> parameters = new ArrayList<>();
-        parameters.add("200.0 2.0 0.0 1.0 2.0 6.0 3.5");
+        parameters.add("1.0 2.0 0.0 1.0 2.0 6.0 3.5");
+        parameters.add("1000.0 2.0 0.0 1.0 2.0 6.0 3.5");//should be filtered and excluded from the result
+        parameters.add("1.0 2.0 0.0 1.0 2.0 6.0 -3000.5");//should be filtered and excluded from the result
         parameters.add("1 2 0 1 2 6 3");
         parameters.add("+1 -2 0 +1 -2 -6 +3");
         parameters.add("1.2 1.0 2   1.2 1.0 7  +7");
         parameters.add("1.2 1.2 10 1.2 1.2 100 7   ");
         List<double[]> expected = new ArrayList<>();
-        expected.add(new double[]{200.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.5});
+        expected.add(new double[]{1.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.5});
         expected.add(new double[]{1.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.0});
         expected.add(new double[]{1.0, -2.0, 0.0, 1.0, -2.0, -6.0, 3.0});
         expected.add(new double[]{1.2, 1.0, 2.0, 1.2, 1.0, 7.0, 7.0});
@@ -46,22 +46,23 @@ public class ConeParserTest {
 
     @Test(dataProvider = "dataForParsing")
     public void parseParameterToArrayTest(List<String> parameter, List<double[]> expected) throws CustomException {
-        List<double[]> actual = new ArrayList<>();
-        for (String line : parameter) {
-            double[] result = parser.parseParameter(line);
-            actual.add(result);
-        }
-        Assert.assertEquals(actual.toArray(), expected.toArray());
+        double[] actual = parser.parseParameter(parameter.get(0));
+        Assert.assertEquals(actual, expected.get(0));
+    }
+
+    @Test(dataProvider = "dataForParsing", expectedExceptions = CustomException.class)
+    public void parseParameterToArrayExceptionTest(List<String> parameter, List<double[]> result) throws CustomException {
+        double[] actual = parser.parseParameter(parameter.get(2));
+    }
+
+    @Test(expectedExceptions = CustomException.class)
+    public void parseParameterToArrayNegativeTest() throws CustomException {
+        String invalidLine = "1er, 2, 3, 4, 5, 6, 7";
+        double[] actual = parser.parseParameter(invalidLine);
     }
 
     @Test(expectedExceptions = CustomException.class)
     public void parseParameterToListExceptionTest() throws CustomException {
         List<double[]> actual = parser.parseParameterToList(null);
-    }
-
-    @Test(expectedExceptions = CustomException.class)
-    public void parseParameterToArrayTest() throws CustomException {
-        String invalidLine = "1er, 2, 3, 4, 5, 6, 7";
-        double[] result = parser.parseParameter(invalidLine);
     }
 }
