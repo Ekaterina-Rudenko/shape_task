1cbf90b8a3213fbc69ea4a801420d3547ffb4726
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-24 07:52:13 +0300

    Update ConeParserTest.java

--- src/test/java/by/epam/task2/test/parser/ConeParserTest.java
+++ src/test/java/by/epam/task2/test/parser/ConeParserTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class ConeParserTest {
     ConeParser parser;
@@ -36,9 +37,31 @@ public class ConeParserTest {
         expected.add(new double[]{1.2, 1.2, 10.0, 1.2, 1.2, 100.0, 7.0});
         return new Object[][]{{parameters, expected}};
     }
+
     @Test(dataProvider = "dataForParsing")
     public void parseParameterToListTest(List<String> parameter, List<double[]> expected) throws CustomException {
         List<double[]> actual = parser.parseParameterToList(parameter);
         Assert.assertEquals(actual.toArray(), expected.toArray());
+    }
+
+    @Test(dataProvider = "dataForParsing")
+    public void parseParameterToArrayTest(List<String> parameter, List<double[]> expected) throws CustomException {
+        List<double[]> actual = new ArrayList<>();
+        for (String line : parameter) {
+            double[] result = parser.parseParameter(line);
+            actual.add(result);
+        }
+        Assert.assertEquals(actual.toArray(), expected.toArray());
+    }
+
+    @Test(expectedExceptions = CustomException.class)
+    public void parseParameterToListExceptionTest() throws CustomException {
+        List<double[]> actual = parser.parseParameterToList(null);
+    }
+
+    @Test(expectedExceptions = CustomException.class)
+    public void parseParameterToArrayTest() throws CustomException {
+        String invalidLine = "1er, 2, 3, 4, 5, 6, 7";
+        double[] result = parser.parseParameter(invalidLine);
     }
 }
