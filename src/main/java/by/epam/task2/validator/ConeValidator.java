1813a03cf4b94b4adedbe9b3efcc48c411c359f7
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-24 03:04:08 +0300

    Update ConeValidator.java

--- src/main/java/by/epam/task2/validator/ConeValidator.java
+++ src/main/java/by/epam/task2/validator/ConeValidator.java
@@ -5,7 +5,6 @@ import by.epam.task2.entity.Point;
 
 public interface ConeValidator {
     int NUMBER_OF_CONE_PARAMETERS = 7;
-    String VALID_CONE_STRING = "([+-]?\\d+((\\.\\d+)|\\.)?\\s){6}[+]?\\d+((\\.\\d+)|\\.)?";
     int INDEX_CENTRE_X = 0;
     int INDEX_CENTRE_Y = 1;
     int INDEX_CENTRE_Z = 2;
@@ -13,8 +12,6 @@ public interface ConeValidator {
     int INDEX_APEX_Y = 4;
     int INDEX_APEX_Z = 5;
     int INDEX_RADIUS = 6;
-
-    boolean checkConeString(String coneDataLine);
 
     boolean isCone(double... parameter);
 
