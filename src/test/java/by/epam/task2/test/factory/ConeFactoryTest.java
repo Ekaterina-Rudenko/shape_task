1c21f3fbd38a26a337181fe83b9729576a5ffa58
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-25 00:29:44 +0300

    Update ConeFactoryTest.java

--- src/test/java/by/epam/task2/test/factory/ConeFactoryTest.java
+++ src/test/java/by/epam/task2/test/factory/ConeFactoryTest.java
@@ -5,33 +5,54 @@ import by.epam.task2.entity.Point;
 import by.epam.task2.exception.CustomException;
 import by.epam.task2.factory.ConeFactory;
 import by.epam.task2.factory.impl.ConeFactoryImpl;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConeFactoryTest {
     ConeFactory coneFactory;
+
     @BeforeClass
-    public void setConeFactory(){
+    public void setConeFactory() {
         coneFactory = new ConeFactoryImpl();
     }
+
     @DataProvider(name = "dataForFactory")
     public Object[][] dataForFactory() {
         List<double[]> parametersList = new ArrayList<>();
-        parametersList.add(new double[]{200.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.5});
-        parametersList.add(new double[]{1.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.0});
+        parametersList.add(new double[]{1.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.5});
+        parametersList.add(new double[]{100.0, 2.0, 0.0, 1.0, 2.0, 6.0, 3.5});//will be filtered as incorrect while creating
+        parametersList.add(new double[]{-1.0, -2.0, 0.0, -1.0, -2.0, 6.0, 3.0});
         parametersList.add(new double[]{1.0, -2.0, 0.0, 1.0, -2.0, -6.0, 3.0});
-        parametersList.add(new double[]{1.2, 1.0, 2.0, 1.2, 1.0, 7.0, 7.0});
-        parametersList.add(new double[]{1.2, 1.2, 10.0, 1.2, 1.2, 100.0, 7.0});
-        return new Object[][]{{parametersList}};
-    }}
-/*    Cone createCone(double[] coordinatesDouble) throws CustomException;
+        parametersList.add(new double[]{1.2, 1.0, 2.0, 1.2, 1.0, 2.0, 7.0});//will be filtered as incorrect while creating
+        parametersList.add(new double[]{100, 1, 10, 100, 1, 100, 7.0});
+        List<Cone> coneList = new ArrayList<>();
+        coneList.add(new Cone(new Point(1.0, 2.0, 0.0), new Point(1.0, 2.0, 6.0), 3.5));
+        coneList.add(new Cone(new Point(-1.0, -2.0, 0.0), new Point(-1.0, -2.0, 6.0), 3.0));
+        coneList.add(new Cone(new Point(1.0, -2.0, 0.0), new Point(1.0, -2.0, -6.0), 3.0));
+        coneList.add(new Cone(new Point(100, 1, 10), new Point(100, 1, 100), 7));
+        return new Object[][]{{parametersList, coneList}};
+    }
 
-    Cone createCone(Point centre, Point apex, double radius) throws CustomException;
+    @Test(dataProvider = "dataForFactory")
+    public void createConeTest(List<double[]> parameterList, List<Cone> expected) throws CustomException {
+        List<Cone> actual = coneFactory.createConeList(parameterList);
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void createConeFromDoublesTest() throws CustomException {
+        Cone actual = coneFactory.createCone(1, 1,2,1,1,5,10);
+        Cone expected = new Cone(new Point(1,1,2), new Point(1,1,5), 10);
+        Assert.assertEquals(actual, expected);
+    }
+    @Test(expectedExceptions = CustomException.class)
+    public void createConeExceptionTest() throws CustomException {
+        Cone actual = coneFactory.createCone(1,2,10,-5,3,20,8);
+    }
 
-    Cone createCone(double centreX, double centreY, double centreZ, double apexX, double apexY, double apexZ, double radius) throws CustomException;
+}
 
-    List<Cone> createConeList(List<double[]> coneParameter) throws CustomException;
-}*/
