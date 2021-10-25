10a0e88af3c09fd67cb08e3a4f0a2832b919a0a2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-25 04:27:52 +0300

    Create WarehouseFillerImpl.java

---
+++ src/main/java/by/epam/task2/warehouse/impl/WarehouseFillerImpl.java
@@ -0,0 +1,29 @@
+package by.epam.task2.warehouse.impl;
+
+import by.epam.task2.entity.Cone;
+import by.epam.task2.service.ConeCalculation;
+import by.epam.task2.service.impl.ConeCalculationImpl;
+import by.epam.task2.entity.ConeParameter;
+import by.epam.task2.warehouse.Warehouse;
+import by.epam.task2.warehouse.WarehouseFiller;
+
+import java.util.List;
+
+public class WarehouseFillerImpl implements WarehouseFiller {
+    public void fillWarehouse(Cone cone) {
+        long id = cone.getConeId();
+        Warehouse warehouse = Warehouse.getInstance();
+        ConeCalculation service = new ConeCalculationImpl();
+        ConeParameter coneParameter = new ConeParameter();
+        double surfaceArea = service.findSurfaceArea(cone);
+        double volume = service.findVolume(cone);
+        coneParameter.setSurfaceArea(surfaceArea);
+        coneParameter.setVolume(volume);
+        warehouse.putParameter(id, coneParameter);
+    }
+    public void fillWarehouse(List<Cone> coneList) {
+        for (Cone cone : coneList) {
+            fillWarehouse(cone);
+        }
+    }
+}
