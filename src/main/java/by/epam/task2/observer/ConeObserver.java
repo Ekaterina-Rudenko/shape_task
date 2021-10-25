45f2f537a5f382e0312120cf9dafe73648b21015
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-25 04:12:39 +0300

    Update ConeObserver.java

--- src/main/java/by/epam/task2/observer/ConeObserver.java
+++ src/main/java/by/epam/task2/observer/ConeObserver.java
@@ -3,7 +3,7 @@ package by.epam.task2.observer;
 import by.epam.task2.exception.CustomException;
 
 public interface ConeObserver {
-    void parameterChanged(ConeEvent coneEvent) throws CustomException;
+    void updateConeParameters(ConeEvent coneEvent) throws CustomException;
     void updateSurfaceArea(ConeEvent coneEvent) throws CustomException;
     void updateVolume(ConeEvent coneEvent) throws CustomException;
 }
