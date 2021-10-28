9c8c5347483054485408224763bbc58ed8935aab
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-28 19:19:22 +0300

    Update ConeParserImpl.java

--- src/main/java/by/epam/task2/parser/impl/ConeParserImpl.java
+++ src/main/java/by/epam/task2/parser/impl/ConeParserImpl.java
@@ -1,6 +1,5 @@
 package by.epam.task2.parser.impl;
 
-import by.epam.task2.exception.CustomException;
 import by.epam.task2.validator.ConeStringValidator;
 import by.epam.task2.validator.ConeValidator;
 import by.epam.task2.validator.impl.ConeStringValidatorImpl;
@@ -10,7 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import by.epam.task2.parser.ConeParser;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -18,37 +17,22 @@ public class ConeParserImpl implements ConeParser {
     static Logger logger = LogManager.getLogger();
 
     @Override
-    public List<double[]> parseParameterToList(List<String> parameterLineList) throws CustomException {
+    public List<double[]> parseParameterToList(List<String> parameterLineList) {
         ConeStringValidator validator = ConeStringValidatorImpl.getInstance();
         ConeValidator coneValidator = ConeValidatorImpl.getInstance();
-        if (parameterLineList == null) {
-            logger.log(Level.ERROR, "The list for parsing is null");
-            throw new CustomException("The list for parsing is null");
+        List<double[]> parameterArrayList = new ArrayList<>();
+        if (parameterLineList != null) {
+            parameterArrayList = parameterLineList.stream()
+                    .filter(validator::checkConeString)
+                    .map(e -> Stream.of(e.split(DELIMITER_SPACE))
+                            .mapToDouble(Double::parseDouble)
+                            .toArray())
+                    .filter(coneValidator::isCone)
+                    .toList();
         }
-        List<double[]> parameterArrayList = parameterLineList.stream()
-                .filter(validator::checkConeString)
-                .map(e -> Stream.of(e.split(DELIMITER_SPACE))
-                        .mapToDouble(Double::parseDouble)
-                        .toArray())
-                .filter(coneValidator::isCone)
-                .toList();
         if (parameterArrayList.isEmpty()) {
-            logger.log(Level.ERROR, "No valid data to parse, the empty list is returned");
+            logger.log(Level.INFO, "No valid data to parse, the empty list is returned");
         }
         return parameterArrayList;
-    }
-
-    @Override
-    public double[] parseParameter(String parameterLine) throws CustomException {
-        ConeStringValidator validator = ConeStringValidatorImpl.getInstance();
-        if (parameterLine == null || !validator.checkConeString(parameterLine)) {
-            logger.log(Level.ERROR, "The line for parsing is null or invalid");
-            throw new CustomException("The line for parsing is null or invalid");
-        }
-        String[] readyToParse = parameterLine.split(DELIMITER_SPACE);
-        double[] coneParameterArray = Arrays.stream(readyToParse)
-                .mapToDouble(Double::parseDouble)
-                .toArray();
-        return coneParameterArray;
     }
 }
