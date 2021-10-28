9af94e34823232e2c2cfd6448884811b0a275afd
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-28 19:19:54 +0300

    Update WarehouseFiller.java

--- src/main/java/by/epam/task2/warehouse/WarehouseFiller.java
+++ src/main/java/by/epam/task2/warehouse/WarehouseFiller.java
@@ -1,10 +1,11 @@
 package by.epam.task2.warehouse;
 
 import by.epam.task2.entity.Cone;
+import by.epam.task2.exception.CustomException;
 
 import java.util.List;
 
 public interface WarehouseFiller {
-    void fillWarehouse(Cone cone);
-    void fillWarehouse(List<Cone> coneList);
+    void fillWarehouse(Cone cone) throws CustomException;
+    void fillWarehouse(List<Cone> coneList) throws CustomException;
 }
