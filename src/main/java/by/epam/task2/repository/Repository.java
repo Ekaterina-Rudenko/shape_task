300e8e3f6447329ce2c6f5fceb47c030385ed6d0
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-28 19:19:37 +0300

    Update Repository.java

--- src/main/java/by/epam/task2/repository/Repository.java
+++ src/main/java/by/epam/task2/repository/Repository.java
@@ -1,6 +1,7 @@
 package by.epam.task2.repository;
 
 import by.epam.task2.entity.Cone;
+import by.epam.task2.exception.CustomException;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,20 +25,29 @@ public class Repository {
         cones = new ArrayList<>();
     }
 
-    public List<Cone> getCones() {
-        return Collections.unmodifiableList(cones);
-    }
 
-    public Optional<Cone> get(int index) {
+    public Optional<Cone> get(int index) throws CustomException {
+        if(index >= size() || index < 0){
+            logger.log(Level.ERROR, "Index " + index + " is out of bounds for repository size " + size());
+            throw new CustomException("Index " + index + " is out of bounds for repository size " + size());
+        }
         Cone cone = cones.get(index);
         return Optional.ofNullable(cone);
+    }
+
+    public List<Cone> getCones() {
+        return Collections.unmodifiableList(cones);
     }
 
     public int size() {
         return cones.size();
     }
 
-    public void setCone(int index, Cone cone) {
+    public void setCone(int index, Cone cone) throws CustomException {
+        if(index >= size() || index < 0){
+            logger.log(Level.ERROR, "Index " + index + " is out of bounds for repository size " + size());
+            throw new CustomException("Index " + index + " is out of bounds for repository size " + size());
+        }
         cones.set(index, cone);
     }
 
