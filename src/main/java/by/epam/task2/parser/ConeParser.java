d1f023921b9191f18c4c86ca007ba4f138aed0c2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-30 01:47:44 +0300

    Update ConeParser.java

--- src/main/java/by/epam/task2/parser/ConeParser.java
+++ src/main/java/by/epam/task2/parser/ConeParser.java
@@ -3,7 +3,5 @@ package by.epam.task2.parser;
 import java.util.List;
 
 public interface ConeParser {
-    String DELIMITER_SPACE = "\\s+";
-
     List<double[]> parseParameterToList(List<String> parameterLineList);
 }
