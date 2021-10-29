a8b872c842a2c01626f6ec1b8520ab319f45c92f
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-30 01:47:55 +0300

    Update ConeStringValidator.java

--- src/main/java/by/epam/task2/validator/ConeStringValidator.java
+++ src/main/java/by/epam/task2/validator/ConeStringValidator.java
@@ -1,6 +1,5 @@
 package by.epam.task2.validator;
 
 public interface ConeStringValidator {
-    String VALID_CONE_STRING = "([+-]?\\d+((\\.\\d+)|\\.)?\\s+){6}[+]?\\d+((\\.\\d+)|\\.)?(\\s+)?";
     boolean checkConeString(String coneDataLine);
 }
