d5ab0b79883572586844c3fb0b1d6a98043b574e
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-30 01:47:50 +0300

    Create ConeConstant.java

---
+++ src/main/java/by/epam/task2/util/ConeConstant.java
@@ -0,0 +1,12 @@
+package by.epam.task2.util;
+
+public final class ConeConstant {
+    public static final int NUMBER_OF_CONE_PARAMETERS = 7;
+    public static final int INDEX_CENTRE_X = 0;
+    public static final int INDEX_CENTRE_Y = 1;
+    public static final int INDEX_CENTRE_Z = 2;
+    public static final int INDEX_APEX_X = 3;
+    public static final int INDEX_APEX_Y = 4;
+    public static final int INDEX_APEX_Z = 5;
+    public static final int INDEX_RADIUS = 6;
+}
